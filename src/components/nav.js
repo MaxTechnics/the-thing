2264232007d96bc767f2194fa47a0e5e4238831f
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2025-09-03 21:34:50 +0200

    Add proper resume

--- src/components/nav.js
+++ src/components/nav.js
@@ -233,7 +233,11 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a
+      className="resume-button"
+      href="/MaximCoppieters_resume.pdf"
+      target="_blank"
+      rel="noopener noreferrer">
       Resume
     </a>
   );
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -69,7 +69,7 @@ const Hero = () => {
     </>
   );
   const five = (
-    <a className="email-link" href="/resume.pdf" target="_blank" rel="noreferrer">
+    <a className="email-link" href="/MaximCoppieters_resume.pdf" target="_blank" rel="noreferrer">
       Check out my resume!
     </a>
   );
